887d790e1ce48c3bade6c0d53e99ba2de21d7c29
benjamin.hil <bhil@tradelab.fr>
2018-07-30 17:55:31 +0200

    delete config

--- config/slack.js
+++
@@ -1,7 +0,0 @@
-module.exports.config = {
-  slack: {
-    user: 'xoxp-405363946708-405363947508-405609997442-42406bce6287b23166d78a7e5a395eaa',
-    app: 'xoxb-405363946708-405374333460-IMHTYk9eXpa2vg9BPHu3VUqO',
-    verification: 'tsehMrxUYkYNldmCAuWM5ZkQ'
-  }
-}
